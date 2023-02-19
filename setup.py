538d6f1a93083af5a9b1fb45d71b1d1567645561
Sanjin84 <ozrobotix@gmail.com>
2023-02-19 17:27:58 +1100

    updated version and imports

--- setup.py
+++ setup.py
@@ -1,36 +1,26 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'findmyfile',         # How you named your package folder (MyLib)
+  packages = find_packages(),   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This package allows you to search a directory for documents that match keywords',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
+  author = 'Sanjin Dedic',                   # Type in your name
   author_email = 'sanjindedic8@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Sanjin84/findmyfile',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Sanjin84/findmyfile/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['file search', 'find file', 'search directory'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'fitz',
-          'pandas',
-          'numpy',
           'python-docx',
           'python-pptx',
-
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3',      
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License', 
+    "Operating System :: OS Independent",  
   ],
+  python_requires=">=3.6",
 )
