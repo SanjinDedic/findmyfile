944f218a693d824253fbe9521deddac3f0eb0891
Sanjin84 <ozrobotix@gmail.com>
2023-06-24 20:13:57 +1000

    refactored main file and added README.

--- NC_findmyfile.py
+++ NC_findmyfile.py
@@ -5,77 +5,46 @@ from docx import Document
 from pptx import Presentation
 import argparse
 import pandas as pd
-
-
 class File:
-    """
-    The base File class represents a generic file object. It holds the path, name, extension and data of the file.
-    It also provides methods to print the file data and search for keywords within the file data.
-    """
-
+    
     def __init__(self, path, extension=".txt"):
         self.path = path
         self.name = os.path.basename(self.path)
         self.extension = extension
         self.readable = True
         self.data = ""
-
-    @classmethod
-    def create(cls, path, extension):
-        return cls(path, extension)
-
-    def print(self, chars=100):
-        """Prints the file data."""
-        print("------------------------------------------------")
-        print(self.data[:chars])
-        print("------------------------------------------------")
-
-    def search_all(self, *keywords):
-        """
-        Searches the file data for the given keyword(s). Returns True if all the keyword(s) are found,
-        otherwise returns False.
-        """
+    def print(self):
+        
+        print(self.data)
+    def search(self, keyword, keyword2=None):
+        
         info = self.data.lower()
-        return all(keyword.lower() in info for keyword in keywords)
-    
-    def search_any(self, *keywords):
-        """
-        Searches the file data for the given keyword(s). Returns True if any of the keyword(s) are found,
-        otherwise returns False.
-        """
-        info = self.data.lower()
-        return any(keyword.lower() in info for keyword in keywords)
-
-    
-    def read(self):
-        pass  # This would be overridden by each subclass    
-
-
+        if keyword2:
+            return keyword.lower() in info and keyword2.lower() in info
+        else:
+            return keyword.lower() in info
 class PptxFile(File):
-    """
-    The PptxFile class represents a PowerPoint (.pptx) file. It inherits from the File class and
-    overrides the read() method to read PowerPoint files.
-    """
+    
+    def __init__(self, path, extension=".pptx"):
+        super().__init__(path, extension)
+        self.read()
     def read(self):
-        """Reads the PowerPoint file and extracts the text."""
+        
         try:
             data = ''
             prs = Presentation(self.path)
-            values = [shape.text for slide in prs.slides for shape in slide.shapes if hasattr(
-                shape, "text")]
+            values = [shape.text for slide in prs.slides for shape in slide.shapes if hasattr(shape, "text")]
             data = ' '.join(values)
             self.data = data
         except:
             self.readable = False
-
-
 class DocxFile(File):
-    """
-    The DocxFile class represents a Word (.docx) file. It inherits from the File class and
-    overrides the read() method to read Word files.
-    """
+    
+    def __init__(self, path, extension=".docx"):
+        super().__init__(path, extension)
+        self.read()
     def read(self):
-        """Reads the Word file and extracts the text."""
+        
         try:
             data = ''
             doc = Document(self.path)
@@ -84,15 +53,13 @@ class DocxFile(File):
             self.data = data
         except:
             self.readable = False
-
-
 class XlsxFile(File):
-    """
-    The XlsxFile class represents an Excel (.xlsx) file. It inherits from the File class and
-    overrides the read() method to read Excel files.
-    """
+    
+    def __init__(self, path, extension=".xlsx"):
+        super().__init__(path, extension)
+        self.read()
     def read(self):
-        """Reads the Excel file and converts the data to a string."""
+        
         try:
             data = ''
             df = pd.read_excel(self.path)
@@ -100,29 +67,25 @@ class XlsxFile(File):
             self.data = data
         except:
             self.readable = False
-
-
 class TxtFile(File):
-    """
-    The TxtFile class represents a text (.txt) file. It inherits from the File class and
-    overrides the read() method to read text files.
-    """
+    
+    def __init__(self, path, extension=".txt"):
+        super().__init__(path, extension)
+        self.read()
     def read(self):
-        """Reads the text file and stores the data as a string."""
+        
         try:
             with open(self.path, 'r', encoding="utf8") as f:
                 self.data = f.read()
         except:
             self.readable = False
-
-
 class PdfFile(File):
-    """
-    The PdfFile class represents a PDF (.pdf) file. It inherits from the File class and
-    overrides the read() method to read PDF files.
-    """
+    
+    def __init__(self, path, extension=".pdf"):
+        super().__init__(path, extension)
+        self.read()
     def read(self):
-        """Reads the PDF file and extracts the text."""
+        
         try:
             doc = fitz.open(self.path)
             text = ""
@@ -131,8 +94,6 @@ class PdfFile(File):
             self.data = text
         except:
             self.readable = False
-
-
 class ProgressBar():
     def __init__(self, total, prefix='', suffix='', decimals=1, length=50, fill='█', print_end="\r"):
         self.total = total
@@ -143,7 +104,6 @@ class ProgressBar():
         self.fill = fill
         self.print_end = print_end
         self.iteration = 0
-
     def print_progress(self, iteration):
         self.iteration = iteration
         percent = ("{0:." + str(self.decimals) + "f}").format(100 *
@@ -154,68 +114,57 @@ class ProgressBar():
               end=self.print_end)
         if self.iteration == self.total:
             print()
-
-
 class FilesDB():
     def __init__(self, path=os.getcwd()):
         self.path = path
         self.files = []
         self.add_file_data()
-
     def count_files(self):
-        """
-        Counts the total number of supported files in the given path.
-        """
+        
         file_count = 0
         for root, dirs, files in os.walk(self.path, topdown=False):
             for file in files:
                 if file.endswith((".pptx", ".docx", ".xlsx", ".pdf", ".txt")):
                     file_count += 1
         return file_count
-
     def add_file_data(self):
-        """
-        Adds file data to the database by creating appropriate file objects for each file type.
-        """
+        
         total_files = self.count_files()
         progress = ProgressBar(
             total_files, prefix='Loading Files:', suffix='Complete', length=50)
         loaded_files = 0
-
         readers = {'pptx': PptxFile,
                    'docx': DocxFile,
                    'xlsx': XlsxFile,
                    'pdf': PdfFile,
                    'txt': TxtFile
                    }
-
         for root, dirs, files in os.walk(self.path, topdown=False):
             for file in files:
                 ext = os.path.splitext(file)[-1].lstrip('.')
                 if ext in readers:
-                    db_file = readers[ext](os.path.join(root, file), extension=ext) #creates a file object
-                    db_file.read() #reads file data into the object
-                    self.files.append(db_file)
+                    data_file = readers[ext](os.path.join(root, file))
+                    self.files.append(data_file)
                     loaded_files += 1
                     progress.print_progress(loaded_files)
-
-    def search(self, *keywords):
+    def search(self, keyword, keyword2=None):
         counter = 0
-        print("Files matching keywords: ", ', '.join(keywords))
+        if keyword2:
+            print("Files matching keywords: ", keyword, keyword2)
+        else:
+            print("Files matching keyword: ", keyword)
         for file in self.files:
             counter += 1
-            if file.search_all(*keywords):
+            if file.search(keyword, keyword2):
                 print(file.name)
         print("\nFiles not readable:")
         for file in self.files:
             if file.readable == False:
                 print(file.name)
         print('Files searched:', counter, '\n')
-
-
 if __name__ == "__main__":
     db = FilesDB()
-    parser = argparse.ArgumentParser(prog="fmf")
+    parser = argparse.ArgumentParser(prog="findmyfile")
     parser.add_argument('-path', '--source_path', type=str, default=os.getcwd(),
                         help="Takes the directory path in quotes as input")
     parser.add_argument('-keyword1', '--text_pattern',
@@ -228,7 +177,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     keyword1 = args.text_pattern
     keyword2 = args.text_pattern2
-    if keyword2 == None:
-        db.search(keyword1)
-    else:
-        db.search(keyword1, keyword2)
+    db.search(keyword1, keyword2)
---
+++ README.md
@@ -0,0 +1,56 @@
+# File Processing Tool 
+
+This project consists of a series of Python classes that can be used to process, search, and display information from different file types (PowerPoint, Word, Excel, PDF, and Text) present in a given directory. 
+
+This tool has been designed to explore a specified directory, read the data from the files present in the directory, and provide a method to search within the file content. It also maintains a count of files that were unreadable due to any issues.
+
+## Dependencies
+
+The following Python packages are used in this project:
+
+- os
+- sys
+- fitz (PyMuPDF)
+- python-docx
+- python-pptx
+- argparse
+- pandas
+
+Please make sure these packages are installed. If not, use the package manager [pip](https://pip.pypa.io/en/stable/) to install them.
+
+```bash
+pip install PyMuPDF python-docx python-pptx pandas
+```
+
+## Classes
+
+The project comprises several classes:
+
+1. **File**: This base class represents a generic file object. It holds the path, name, extension, and data of the file. It also provides methods to print the file data and search for keywords within the file data. The `read` method of this class should be overridden by each subclass to handle specific file types.
+
+2. **PptxFile, DocxFile, XlsxFile, TxtFile, PdfFile**: These classes represent specific file types (PowerPoint, Word, Excel, Text, and PDF files, respectively). Each of them inherits from the base File class and overrides the `read` method to handle the specific file type.
+
+3. **ProgressBar**: This class provides a simple progress bar which can be used to visualize the progress of long running operations.
+
+4. **FilesDB**: This class represents a database of files. It explores a given directory path and creates an instance of the appropriate class for each supported file type (pptx, docx, xlsx, pdf, txt) it finds. It provides a method to search for keywords across all files in the database.
+
+## Usage
+
+This project can be utilized as a library in your project or as a standalone script. The usage would depend on the specific requirements of your project. 
+
+An example of searching for keywords would be:
+
+```python
+fdb = FilesDB('your/directory/path')
+fdb.search('your', 'keywords')
+```
+
+This will print the names of the files which contain all the keywords.
+
+## Contributions
+
+Contributions are always welcome! Please read the contribution guidelines first.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
